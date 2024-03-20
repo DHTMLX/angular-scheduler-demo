db21241a615a40adc28c4f2757954b9e25afdecd
Artyom Borisevich <aborisevich@xbsofware.by>
2024-03-20 16:31:34 +0300

    updated Scheduler component

--- src/app/components/scheduler.component.ts
+++ src/app/components/scheduler.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
-import { scheduler } from "dhtmlx-scheduler";
+import { Scheduler, SchedulerStatic } from "@dhx/trial-scheduler";
 import { EventService } from "../services/event.service";
 
 
@@ -14,33 +14,30 @@ import { EventService } from "../services/event.service";
 
 export class SchedulerComponent implements OnInit {
     @ViewChild("scheduler_here", { static: true }) schedulerContainer: ElementRef;
-
+    private _scheduler?: SchedulerStatic;
     constructor(private eventService: EventService) { }
 
     ngOnInit() {
-
+        let scheduler = Scheduler.getSchedulerInstance();
         scheduler.config.date_format = '%Y-%m-%d %H:%i';
 
-        scheduler.init(this.schedulerContainer.nativeElement, new Date(2022, 8, 1));
-        scheduler.createDataProcessor((entity: any, action: any, data: any, id: string | number) => {
-            switch (action) {
-                case "create":
-                    return this.eventService.insert(data);
-                    break;
-                case "update":
-                    return this.eventService.update(data);
-                    break;
-                case "delete":
-                    return this.eventService.remove(data);
-                    break;
+        scheduler.init(this.schedulerContainer.nativeElement, new Date(2024, 9, 1));
+        const dp = scheduler.createDataProcessor({
+            event: {
+                create: (data: any) => this.eventService.insert(data),
+                update: (data: any) => this.eventService.update(data),
+                delete: (id: number) => this.eventService.remove(id),
             }
-            throw new Error(`Action [${action}] is not implemented`);
         });
 
         this.eventService.get()
             .then((data) => {
                 scheduler.parse(data);
             });
-
+        this._scheduler = scheduler;
+    }
+    
+    ngOnDestroy() {
+        if (this._scheduler) this._scheduler.destructor();
     }
 }
--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -3,8 +3,8 @@ import {InMemoryDbService} from "angular-in-memory-web-api";
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         let events = [
-            {id: 1, start_date: "2022-09-01 06:00", end_date: "2022-09-01 13:00", text: "Event 1"},
-            {id: 2, start_date: "2022-09-03 07:00", end_date: "2022-09-03 12:00", text: "Event 2"}
+            {id: 1, start_date: "2024-10-01 06:00", end_date: "2024-10-01 13:00", text: "Event 1"},
+            {id: 2, start_date: "2024-10-03 07:00", end_date: "2024-10-03 12:00", text: "Event 2"}
         ];
     return {events};
     }
